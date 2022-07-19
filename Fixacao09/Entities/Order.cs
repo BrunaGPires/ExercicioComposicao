044cdf4c2475758b05aa20bc787ccf6cb967960f
Bruna Pires <bruna.gpires@hotmail.com>
2022-07-19 14:30:28 -0300

    Exercicio solucionado

---
+++ Fixacao09/Entities/Order.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Fixacao09.Entities.Enums;
+
+namespace Fixacao09.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrdemItem> Items{ get; set; } = new List<OrdemItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrdemItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrdemItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double calc = 0.0;
+            foreach(OrdemItem item in Items)
+            {
+                calc += item.SubTotal();
+            }
+            return calc;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach(OrdemItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total Price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Fixacao09/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fixacao09.Entities;
+using Fixacao09.Entities.Enums;
+using System.Globalization;
+
+namespace Fixacao09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string clName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = (OrderStatus)Enum.Parse(typeof(OrderStatus),Console.ReadLine());
+            //
+            Client client = new Client(clName, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.WriteLine();
+            Console.Write("How many items to this order? ");
+            int itemNum = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= itemNum; i++)
+            {
+                Console.WriteLine($"Enter #{i} data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                //
+                Product product = new Product(productName, price);
+                
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                //
+                OrdemItem ordemItem = new OrdemItem(quantity, price, product);
+                order.AddItem(ordemItem);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMARY: ");
+            Console.WriteLine(order);
+        }
+    }
+}
